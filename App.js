dcffd4abf3299f02cc9efaa9775ecdbf79df99f7
tejaswiniborra28 <tejaswiniborra28@gmail.com>
2023-09-10 18:12:24 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,82 +1,38 @@
-/*
-*
-*   <div id="parent">
-*    <div id="child">
-*    <h1> hello </h1>
-*    </div>
-*    </div>
-ReactElement(object) => HTML(Browser understands)
-*
-*/
-// React elements nothing but js object and render converts react elements to the html elements which browser understands
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },
-//     React.createElement("h1", {}, "Hello world!")
-//   )
-// );
-
-//////////////////////////////////////////////////////////////
-
-/*
-*
-*   <div id="parent">
-*    <div id="child">
-*    <h1> i am h1 tag </h1>
- <h2> i am h2 tag </h2>
-*    </div>
-*    </div>
-ReactElement(object) => HTML(Browser understands)
-*
-*/
-
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement("div", { id: "child" }, [
-//     React.createElement("h1", {}, "i am h1 tag!"),
-//     React.createElement("h2", {}, "I am h2 tag!"),
-//   ])
-// );
-
-//////////////////////////////////////////////////////////////
-
-/*
-*
-*   <div id="parent">
-*    <div id="child1">
-*          <h1> i am h1 tag 1</h1>
-           <h2> i am h2 tag 2</h2>
-*    </div>
-      <div id="child2">
-*          <h1> i am h1 tag 1</h1>
-           <h2> i am h2 tag 2</h2>
-*    </div>
-*    </div>
-ReactElement(object) => HTML(Browser understands)
-*
-*/
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "i am h1 tag!"),
-    React.createElement("h2", {}, "I am h2 tag!"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "i am h1 tag!"),
-    React.createElement("h2", {}, "I am h2 tag!"),
-  ]),
-]);
-console.log(parent); // object
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World from React!"
+//jsx-- Html like or Xml like syntax
+// jsx => React.createElement => React element (js object) => HTML elememt ( render )
+
+// ---- single line-----
+// const jsxHeading = <h1 id="heading">Nameste React</h1>;
+
+//--- multi line-----
+const Heading = (
+  <h1 className="heading" tabIndex="5">
+    Nameste React
+  </h1>
 );
+
+const Title = () => <h1 className="title">Namaste react title</h1>;
+
+// React Functional components -- normal js func which returns jsx or react elements
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title />
+      {Title()}
+      {console.log("nameste react console")}
+      <h1>Namaste react functional component</h1>
+    </div>
+  );
+};
+console.log(Heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+// RENDERing react element
+root.render(Heading);
+
+// rendering react component
+root.render(<HeadingComponent />);
